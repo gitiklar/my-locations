ba94903571efed85eb2e31d8c3cc445223cbcf29
gitiklar <65737909+gitiklar@users.noreply.github.com>
2021-05-02 15:26:06 +0300

    renamed

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ const indexPath  = path.join(__dirname, 'public/index.html');
 app.use(publicPath);
 app.get('/', function (_, res) { res.sendFile(indexPath) });
 app.get('*', function (_, res) { res.sendFile(indexPath) });
-
-// use the express-static middleware
-//app.use(express.static("public"))
-// define the first route
 /*
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
